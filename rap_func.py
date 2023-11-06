efb6fc25377a6f4d6506698925729d0ae548232c
bobbysaba <132299779+bobbysaba@users.noreply.github.com>
2023-11-06 12:28:49 -0600

    accepts multiple resolutions

--- rap_func.py
+++ rap_func.py
@@ -5,13 +5,14 @@ import xarray as xr
 from siphon.catalog import TDSCatalog
 
 # define function to get rap data (13km gridpsace)
-def get_rap(start, end, data_path, bounds = False, variables= False, nc_path = False, dict_path = False):
+def get_rap(res, start, end, bounds = False, variables= False, nc_path = False, dict_path = False):
+    # res: accepts integer values of 13 or 20 for the desired resolution of the RAP data
     # start: list of datetime objects indicating the start day/time for each provided period
     # end: list of datetime objects indicating the end day/time for each provided period
+        # start and end times should be in UTC
     # variables: list of variables that you would like to pull from the data files
         # All variables will be collected if False
         # find an example file with vars here: https://ruc.noaa.gov/ruc/ruc2vars.html
-    # data_path: path to store pickle file/nc files with RAP data
     # bounds: list of desired coordinate bounds
         # bounds are [west, east, south, north]
         # FORMATTING: lon values range from -139 to -57
@@ -21,6 +22,17 @@ def get_rap(start, end, data_path, bounds = False, variables= False, nc_path = F
     # save_dict: path to save dictionary pickle file
         # false: no pickle file is created
     # returns dictionary of rap data
+    
+    # determine the resoltuion URL code
+    if res == 13:
+        res_code = '130'
+    if res == 20:
+        res_code = '252'
+        
+    # quit the function if the resolution is not valid
+    if res != 13 and res != 20:
+        print('Invalid resolution provided...quitting function.')
+        return()
 
     # create returned dict with data
     rap_data = {}
@@ -44,23 +56,30 @@ def get_rap(start, end, data_path, bounds = False, variables= False, nc_path = F
             date1 = str(t.year) + '{:02d}'.format(t.month)
             date2 = date1 + '{:02d}'.format(t.day)
             
-            # create a dict layer with the day/time
-            rap_data[date2] = {}
+            # create a dict layer with the day/time if needed
+            if date2 not in rap_data:
+                rap_data[date2] = {}
+            
+            # if the day/time has already been pulled, move to the next time
+            if '{:02d}'.format(t.hour) in rap_data[date2]:
+                continue
+            
+            # create the necessary dictionary layer
             rap_data[date2]['{:02d}'.format(t.hour)] = {}
             
             # try the new AND old catelogs to find the file
             try:
-                cat = TDSCatalog('https://www.ncei.noaa.gov/thredds/catalog/model-rap130anl/' + date1 + '/' + date2 + '/catalog.xml')
+                cat = TDSCatalog('https://www.ncei.noaa.gov/thredds/catalog/model-rap' + res_code + 'anl/' + date1 + '/' + date2 + '/catalog.xml')
             except:
                 try:
-                    cat = TDSCatalog('https://www.ncei.noaa.gov/thredds/catalog/model-rap130anl-old/' + date1 + '/' + date2 + '/catalog.xml')
+                    cat = TDSCatalog('https://www.ncei.noaa.gov/thredds/catalog/model-rap' + res_code + 'anl-old/' + date1 + '/' + date2 + '/catalog.xml')
                 except:
                     print('Invalid URL on ' + t.strftime('%Y%m%d @ %H:%M:%S'))
                     continue
 
             # pull the data file
             try:
-                ds = cat.datasets[str('rap_130_' + date2 + '_' + '{:02d}'.format(t.hour) + '00' + '_000.grb2')]
+                ds = cat.datasets['rap_' + res_code + '_' + date2 + '_' + '{:02d}'.format(t.hour) + '00' + '_000.grb2']
             except:
                 print('No data exists on ' + t.strftime('%Y%m%d @ %H:%M:%S'))
                 continue
@@ -94,8 +113,8 @@ def get_rap(start, end, data_path, bounds = False, variables= False, nc_path = F
                 # save nc file (overwriting previous file if applicable)
                 xr_nc.to_netcdf(nc_path + date2 + '_' + '{:02d}'.format(t.hour) + '.nc', mode = 'w')
             
-            # close nc files
-            xr_nc.close()
+                # close nc files
+                xr_nc.close()
             
             # print the RAP data processed
             print('RAP data on ' + date2 + ' @ ' + '{:02d}'.format(t.hour) + ' has been processed.')
